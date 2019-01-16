b2ba80a42e8e54433462d0c1bf8d043a4f571729
Aslan <berezg86@gmail.com>
2019-01-17 01:17:14 +0300

    верстка второй страницы

--- js/common.js
+++ js/common.js
@@ -1,22 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	// Слайдер swiper
-	// var swiper = new Swiper('.swiper-container', {
-	// 	pagination: '.swiper-pagination',
-	// 	effect: 'coverflow',
-	// 	grabCursor: true,
-	// 	centeredSlides: true,
-	// 	loop: true,
-	// 	spaceBetween: -250,
-	// 	slidesPerView: 'auto',
-	// 	coverflow: {
-	// 		rotate: 50,
-	// 		stretch: 0,
-	// 		depth: 100,
-	// 		modifier: 1,
-	// 		slideShadows : true,
-	//
-	// 	}
-	// });
+    var owl = $("#brends-slider");
+    owl.owlCarousel({
+        loop: true,
+        nav: true,
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        center: false, //если нужны обрезаные края
+        navText: ['<span class="nav-left"></span>', '<span class="nav-right"></span>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            1200: {
+                items: 5
+            },
+        }
+    });
 
-});//END READY
+	$('#logIn').click(function(){
+		$('.overlay').fadeIn();
+		$('.form-modal').fadeIn();
+		$('.form-modal_close').click(function(){
+			$('.overlay').fadeOut();
+			$('.form-modal').fadeOut();
+		});
+		$('.overlay').click(function(){
+			$('.overlay').fadeOut();
+			$('.form-modal').fadeOut();
+		});
+	});
+
+}); //END READY

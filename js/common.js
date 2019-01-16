8c4ea72c284f96f8dfbdf0427c2276ab097eefb9
Aslan <berezg86@gmail.com>
2019-01-16 20:12:08 +0300

    разметка сервис

---
+++ js/common.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	// Слайдер swiper
+	// var swiper = new Swiper('.swiper-container', {
+	// 	pagination: '.swiper-pagination',
+	// 	effect: 'coverflow',
+	// 	grabCursor: true,
+	// 	centeredSlides: true,
+	// 	loop: true,
+	// 	spaceBetween: -250,
+	// 	slidesPerView: 'auto',
+	// 	coverflow: {
+	// 		rotate: 50,
+	// 		stretch: 0,
+	// 		depth: 100,
+	// 		modifier: 1,
+	// 		slideShadows : true,
+	//
+	// 	}
+	// });
+
+});//END READY
